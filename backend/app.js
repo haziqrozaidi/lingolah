9cfd497c60fa55a72abb3ac8c1c78a2a230bb214
Haziq Rozaidi <182204532+haziqrozaidi@users.noreply.github.com>
2025-05-18 03:20:26 +0800

    Set up Clerk authentication in backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { clerkMiddleware } = require('@clerk/express');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(clerkMiddleware());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
