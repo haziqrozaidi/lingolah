6efce518dd9ecb2e113e59a882e98fb317cc3920
Haziq Rozaidi <182204532+haziqrozaidi@users.noreply.github.com>
2025-05-18 01:29:13 +0800

    Set up Clerk authentication in frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,18 @@ import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { clerkPlugin } from '@clerk/vue'
 import PrimeVue from 'primevue/config'
 import Noir from './presets/Noir.js'
 
 import App from './App.vue'
 import router from './router'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 const app = createApp(App)
 
@@ -18,5 +25,6 @@ app.use(PrimeVue, {
     preset: Noir,
   },
 })
+app.use(clerkPlugin, { publishableKey: PUBLISHABLE_KEY })
 
 app.mount('#app')
