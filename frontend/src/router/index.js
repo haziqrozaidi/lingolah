cce39884adbb78075b829f842451bcfbb3c72ea7
Haziq Rozaidi <182204532+haziqrozaidi@users.noreply.github.com>
2025-05-21 02:21:50 +0800

    Make sidebar and header fixed

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LearnView from '../views/flashcard/LearnView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import ReviewView from '../views/flashcard/ReviewView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'dashboard',
+      component: DashboardView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/learn',
+      name: 'learn',
+      component: LearnView,
+    },
+    {
+      path: '/review',
+      name: 'review',
+      component: ReviewView,
     },
   ],
 })
