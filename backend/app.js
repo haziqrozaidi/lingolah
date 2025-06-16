2e1f4675ef52d5bf42fb06155b7bcf04a66fc76d
Haziq Rozaidi <182204532+haziqrozaidi@users.noreply.github.com>
2025-06-16 23:46:57 +0800

    Set up CORS

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const { clerkMiddleware } = require('@clerk/express');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(clerkMiddleware());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
