c72c4721743aa58f61065288bbfc4d0b7b84f88a
Syahmi <syahmimuhd30@gmail.com>
2025-06-23 14:00:13 +0800

    change the schema of comment model to allow multiple comment per user instead of one comment per user

---
+++ backend/prisma/migrations/20250623025317_modify_comment_add_id_as_pk/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - The primary key for the `Comment` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The required column `id` was added to the `Comment` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- AlterTable
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_pkey",
+ADD COLUMN     "id" TEXT NOT NULL,
+ADD CONSTRAINT "Comment_pkey" PRIMARY KEY ("id");
