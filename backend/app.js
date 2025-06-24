e5afa79e570bf57cb80bd4ab0f2fb75071418f85
Syahmi <syahmimuhd30@gmail.com>
2025-06-25 02:45:27 +0800

    move the community and admin forum before mounting cors to ensure the route cors is used

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,8 @@ var adminDashboard = require('./routes/adminDashboard');
 
 var app = express();
 
+app.use('/api/community', communityRouter);
+app.use('/api/admin/forum', adminForumRouter);
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,8 +32,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/clerk-user', clerkUserRouter);
 app.use('/api/forum', forumRouter);
-app.use('/api/admin/forum', adminForumRouter);
-app.use('/api/community', communityRouter);
 app.use('/flashcard-sets', flashcardSetsRouter);
 app.use('/flashcards', flashcardRoutes);
 app.use('/api/admin', adminDashboard);
